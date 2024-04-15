99901df9cf2979f7363effcd6b340534c5d3ba49
huY-it-now <lehuy15062003@gmai.com>
2024-04-15 16:58:52 +0700

    add api users login and register

---
+++ Angular + Java Spring Boot/shopp-app/back-end/shopapp/src/main/java/com/project/shopapp/dtos/UserLoginDTo.java
@@ -0,0 +1,21 @@
+package com.project.shopapp.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginDTo {
+    @JsonProperty("phone_number")
+    @NotBlank(message = "Phone number is required")
+    private String phoneNumber;
+
+    private String address;
+
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+}
