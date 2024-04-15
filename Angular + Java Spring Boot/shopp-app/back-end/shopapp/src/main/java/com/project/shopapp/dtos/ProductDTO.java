f635c9a9f4ef42d230f7d290edbc0aa25aaacb9a
huY-it-now <lehuy15062003@gmai.com>
2024-04-15 12:13:42 +0700

    add product backend

--- Angular + Java Spring Boot/shopp-app/back-end/shopapp/src/main/java/com/project/shopapp/dtos/ProductDTO.java
+++ Angular + Java Spring Boot/shopp-app/back-end/shopapp/src/main/java/com/project/shopapp/dtos/ProductDTO.java
@@ -6,6 +6,9 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Data
 @Getter
@@ -20,11 +23,14 @@ public class ProductDTO {
     @Min(value = 0, message = "Price must be greater than or equal to 0")
     @Max(value = 10000000, message = "Price must be less than or equal to 10,000,000")
     private Float price;
+
     private String thumbnail;
 
-    @JsonProperty("description")
     private String description;
 
     @JsonProperty("category_id")
     private String categoryId;
+
+    private List<MultipartFile> files;
+
 }
