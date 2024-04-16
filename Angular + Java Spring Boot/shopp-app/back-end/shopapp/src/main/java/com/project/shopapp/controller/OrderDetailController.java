e0217b2a2a0cdb63e6d504ccb8dee8dfdcd9e5a6
huY-it-now <lehuy15062003@gmai.com>
2024-04-16 11:16:46 +0700

    add orderDetail

--- Angular + Java Spring Boot/shopp-app/back-end/shopapp/src/main/java/com/project/shopapp/controller/OrderDetailController.java
+++ Angular + Java Spring Boot/shopp-app/back-end/shopapp/src/main/java/com/project/shopapp/controller/OrderDetailController.java
@@ -13,4 +13,38 @@ import java.util.List;
 @RestController
 @RequestMapping("${api.prefix}/order_details")
 public class OrderDetailController {
+    //Thêm mới 1 orer detail
+    @PostMapping
+    public ResponseEntity<?> createOrderDetail(
+            @Valid @RequestBody OrderDetailDTO orderDetailDTO
+    ) {
+        return ResponseEntity.ok("createOrderDetail here");
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOrderDetail(
+            @Valid @PathVariable("id") Long id
+    ) {
+        return ResponseEntity.ok("getOrderDetail with id = " + id);
+    }
+    //Lấy ra danh sách các order_details của 1 order nào đó
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<?> getOrderDetails(
+            @Valid @PathVariable("orderId") Long orderId
+    ) {
+        return ResponseEntity.ok("getOrderDetails with orderId = " + orderId);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateOrderDetail(
+            @Valid @PathVariable("id") Long id,
+            @RequestBody OrderDetailDTO newOrderDetailData
+    ) {
+        return ResponseEntity.ok("updateOrderDetail with id = " + id + ",newOrderDetailData: "
+        + newOrderDetailData);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrderDetail(
+            @Valid @PathVariable("id") Long id
+    ) {
+        return ResponseEntity.noContent().build();
+    }
 }
--- Angular + Java Spring Boot/shopp-app/back-end/shopapp/src/main/java/com/project/shopapp/dtos/OrderDetailDTO.java
+++ Angular + Java Spring Boot/shopp-app/back-end/shopapp/src/main/java/com/project/shopapp/dtos/OrderDetailDTO.java
@@ -1,6 +1,7 @@
 package com.project.shopapp.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
 import lombok.*;
 
 @Data
@@ -11,17 +12,22 @@ import lombok.*;
 @NoArgsConstructor
 public class OrderDetailDTO {
     @JsonProperty("order_id")
+    @Min(value = 1, message = "Order's ID must be > 0")
     private Long orderId;
 
     @JsonProperty("product_id")
+    @Min(value = 1, message = "Product's ID must be > 0")
     private Long productId;
 
+    @Min(value = 0, message = "price must be >= 0")
     private Float price;
 
     @JsonProperty("number_of_product")
+    @Min(value = 1, message = "number_of_product must be >= 1")
     private int numberOfProduct;
 
     @JsonProperty("total_money")
+    @Min(value = 0, message = "Total money must be >= 0")
     private Float totalMoney;
 
     private String color;
